411e284993e81aab18cca601874c1e4b3f8a3221
jjeejj <wenjunjiang1993@163.com>
2016-11-19 21:44:48 +0800

    open

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); //单独输出一个文件
+const OpenBrowserPlugin = require('open-browser-webpack-plugin'); //打开浏览器插件
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+
+    entry: [
+        'webpack-dev-server/client/?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
     output: {
         path: 'build',
         filename: 'bundle.js'
@@ -24,6 +31,11 @@ module.exports = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin("style.css")
+        new ExtractTextPlugin("style.css"),
+        new OpenBrowserPlugin({
+            url:'http://localhost:8080'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+
     ]
 }
